8a62961fb9c541212e5e1243eb60d9aa26a7e309
kuma <kuma@sonicgarden.jp>
2025-12-11 13:40:55 +0900

    feat: ユーザー認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate!
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate!
+    auth_header = request.headers["Authorization"]
+    token = auth_header&.split(" ")&.last
+
+    @current_user = User.find_by(api_token: token)
+
+    unless @current_user
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
+  end
 end
